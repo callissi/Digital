4d1fb2a6f76b9b0ea68b0f7fb5aa3a605ff87682
Adeodu Caleb <69051843+callissi@users.noreply.github.com>
2023-08-27 18:06:35 +0100

    Admin

---
+++ about.php
@@ -0,0 +1,335 @@
+<?php include'https.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <title>NuKreationz Digital Solutions Website | About Us</title>
+    <!-- Stylesheets -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&amp;family=Teko:wght@300;400;500;600;700&amp;display=swap"
+        rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/owl.css" rel="stylesheet">
+    <link href="css/flaticon.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/jquery-ui.css" rel="stylesheet">
+    <link href="css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="css/hover.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/jarallax.css">
+    <link href="css/custom-animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- rtl css -->
+    <link href="css/rtl.css" rel="stylesheet">
+    <!-- Responsive File -->
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!-- Color css -->
+    <link rel="stylesheet" id="jssDefault" href="css/colors/color-default.css">
+
+
+    <link rel="shortcut icon" href="images/faviconmixed.png" id="fav-shortcut" type="image/x-icon">
+    <link rel="icon" href="images/faviconmixed.png" id="fav-icon" type="image/x-icon">
+
+    <!-- Responsive Settings -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+
+    <div class="page-wrapper">
+
+       
+
+        <!-- Main Header -->
+        <?php include'header.php'; ?>
+        <!-- End Main Header -->
+
+        <!--Mobile Menu-->
+        <?php include'mobile_menu.php'; ?>
+        <!-- /.side-menu__block -->
+
+        <!--Search Popup-->
+        <?php include'popup.php'; ?>
+        <!-- /.search-popup -->
+
+        <!-- Banner Section -->
+        <section class="page-banner">
+            <div class="image-layer" style="background-image:url(images/resource/digital_we.jfif);"></div>
+            <div class="shape-1"></div>
+            <div class="shape-2"></div>
+            <div class="banner-inner">
+                <div class="auto-container">
+                    <div class="inner-container clearfix">
+                        <h1>About Us</h1>
+                        <div class="page-nav">
+                            <ul class="bread-crumb clearfix">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">About Us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--End Banner Section -->
+
+
+        <section class="featured-section featured-section__about-two">
+            <div class="auto-container">
+                <div class="row clearfix">
+                    <!--Left Column-->
+                    <div class="left-col col-lg-6 col-md-12 col-sm-12">
+                        <div class="inner wow fadeInLeft animated" data-wow-delay="0ms" data-wow-duration="1500ms"
+                            style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+                            <div class="image-box"><img src="images/resource/digi_us.jpg" alt=""></div>
+                        </div>
+                    </div>
+                    <!--Right Column-->
+                    <div class="right-col col-lg-6 col-md-12 col-sm-12">
+                        <div class="inner">
+                            <div class="sec-title">
+                                <h2>We Build and Develop Digital Solutions<span class="dot">.</span></h2>
+                                <div class="lower-text">We are a digital marketing agency in Lagos that delivers game-changing results. We help businesses get exposure to more people and drive sales.
+
+We craft and execute effective digital marketing campaigns backed by honest data. Data that we build over-time helps us create better campaigns. This ensures that our campaigns convert and affect the top-line growth of our partners.
+<br><br>
+With Nukreationz Digital, you get a dedicated partner who plans and executes strategies that drive your business to perpetual growth.
+<br>
+Our certainty of getting results is backed by the collective years of experience of our team of;
+<br><br>
+Social Media Managers<br>
+Search Engine Optimists<br>
+Web Developers<br>
+Brand Strategists<br>
+Online Ad Experts<br>
+Customer Service Specialists and;<br>
+Project Managers
+                                </div>
+                            </div>
+                            <div class="features">
+                                <div class="row clearfix">
+                                    <div class="feature col-md-6 col-sm-12">
+                                        <div class="inner-box">
+                                            <a href="contact.php"><h6>Book a Consultation</h6></a>
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="feature col-md-6 col-sm-12">
+                                        <div class="inner-box">
+                                            <a href="index.php#pricing_one"><h6>Order a Service</h6></a>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="feature-six">
+            <div class="auto-container">
+                <div class="row no-gutters">
+                    <div class="col-md-12 col-lg-12 col-xl-6">
+                        <div class="feature-six__image clearfix">
+                            <img src="images/resource/we_home.jpg"
+                                alt="Total design freedom for everyone">
+                            <div class="feature-six__image__caption">The best global growth agency.</div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-12 col-xl-6 d-flex">
+                        <div class="my-auto">
+
+                            <div class="feature-six__content">
+                                <div class="sec-title">
+                                    <h2>Reach your customers in every market<span class="dot">.</span></h2>
+                                </div>
+                                <div class="feature-six__content__text">
+                                    We bring together market insights, years of optimization expertise and a unique focus on understanding your audience and business. By combining this, we create a measurable and actionable paid media strategy that accelerates you toward your goals – no matter how ambitious. </div>
+                                <!-- /.feature-six__content__text -->
+                                <ul class="m-0 list-unstyled feature-six__list">
+                                    <li class="feature-six__list__item">
+                                        <i class="flaticon-check-symbol"></i>
+                                        Grow your brand with paid Media</li>
+                                    <li class="feature-six__list__item">
+                                        <i class="flaticon-check-symbol"></i>
+                                        Flexible and budget-friendly list of services</li>
+                                    <li class="feature-six__list__item">
+                                        <i class="flaticon-check-symbol"></i>
+                                        Bespoke solutions that would get you results</li>
+                                    <li class="feature-six__list__item">
+                                        <i class="flaticon-check-symbol"></i>
+                                        Experience, insight & the right blend of creativity</li>
+                                </ul>
+                            </div><!-- /.feature-six__content -->
+                        </div><!-- /.my-auto -->
+                    </div><!-- /.col-md-12 col-lg-6 -->
+                </div><!-- /.row -->
+            </div><!-- /.auto-container -->
+        </section>
+
+
+        <section class="feature-seven">
+            <div class="auto-container">
+                <div class="sec-title centered">
+                    <h2>Some of our Services<span class="dot">.</span></h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-lg-4">
+                        <div class="feature-seven-card">
+                            <div class="feature-seven-card__inner">
+                                <div class="feature-seven-card__image">
+                                    <img src="images/resource/blog1.jpg" alt="">
+                                </div>
+                                <!-- /.feature-seven-card__image -->
+                                <div class="feature-seven-card__content">
+                                    <h3 class="feature-seven-card__title">
+                                        <a href="social_media_marketing.php">Social Media Marketing Services</a>
+                                    </h3>
+                                    <p class="feature-seven-card__text">
+                                        Nukreationz Digital is one of Nigeria's leading digital marketing and branding company.
+                                    </p>
+                                </div><!-- /.feature-seven-card__content -->
+                            </div><!-- /.feature-seven-card__inner -->
+                        </div><!-- /.feature-seven-card -->
+                    </div><!-- /.col-md-12 -->
+                    <div class="col-md-12 col-lg-4">
+                        <div class="feature-seven-card">
+                            <div class="feature-seven-card__inner">
+                                <div class="feature-seven-card__image">
+                                    <img src="images/resource/blog2.jpg" alt="">
+                                </div>
+                                <!-- /.feature-seven-card__image -->
+                                <div class="feature-seven-card__content">
+                                    <h3 class="feature-seven-card__title">
+                                        <a href="web_development.php">Website Development Services</a>
+                                    </h3>
+                                    <p class="feature-seven-card__text">
+                                        Nukreationz Digital is one of Nigeria's leading website design and development company.
+                                    </p>
+                                </div><!-- /.feature-seven-card__content -->
+                            </div><!-- /.feature-seven-card__inner -->
+                        </div><!-- /.feature-seven-card -->
+                    </div><!-- /.col-md-12 -->
+                    <div class="col-md-12 col-lg-4">
+                        <div class="feature-seven-card">
+                            <div class="feature-seven-card__inner">
+                                <div class="feature-seven-card__image">
+                                    <img src="images/resource/blog3.jpg" alt="">
+                                </div>
+                                <!-- /.feature-seven-card__image -->
+                                <div class="feature-seven-card__content">
+                                    <h3 class="feature-seven-card__title">
+                                        <a href="#">Brand Identity and Graphics Design</a>
+                                    </h3>
+                                    <p class="feature-seven-card__text">
+                                        We help to develop a powerful and consistent brand identity for your business.
+                                    </p>
+                                </div><!-- /.feature-seven-card__content -->
+                            </div><!-- /.feature-seven-card__inner -->
+                        </div><!-- /.feature-seven-card -->
+                    </div><!-- /.col-md-12 -->
+                </div><!-- /.row -->
+            </div><!-- /.auto-container -->
+            <hr>
+        </section>
+
+        <section class="testimonials-five">
+            <div class="auto-container">
+                <div class="sec-title text-center">
+                    <h2>Our testimonials <span class="dot">.</span></h2>
+                </div><!-- /.sec-title -->
+                <div class="testimonials-carousel-two owl-theme owl-carousel testimonials-five__carousel">
+                    <div class="item">
+                        <div class="testimonials-five-card">
+                            <img src="images/resource/user.png" alt=""
+                                class="testimonials-five-card__image">
+                            <p class="testimonials-five-card__text">Hello everyone! Simply put, I love the new website!!! Its coming up very nice. The color play impressive etc.</p>
+                            <h3 class="testimonials-five-card__title">
+                                Dr Olubanke Ilo
+                            </h3><!-- /.testimonials-five-card__title -->
+                            <span class="testimonials-five-card__designation">
+                                MD and Senior Ophthalmologist, Choice Eye Clinic
+                            </span><!-- /.testimonials-five-card__designation -->
+                        </div><!-- /.testimonials-five-card -->
+                    </div><!-- /.item -->
+                    <div class="item">
+                        <div class="testimonials-five-card">
+                            <img src="images/resource/user.png" alt=""
+                                class="testimonials-five-card__image">
+                            <p class="testimonials-five-card__text">Thanks to Nukreationz Printing Solutions for helping us with our social media management.</p>
+                            <h3 class="testimonials-five-card__title">
+                                Collins Ikemefuna
+                            </h3><!-- /.testimonials-five-card__title -->
+                            <span class="testimonials-five-card__designation">
+                                Web Developer at Paddysco Sports
+                            </span><!-- /.testimonials-five-card__designation -->
+                        </div><!-- /.testimonials-five-card -->
+                    </div><!-- /.item -->
+                    
+                </div><!-- /.testimonials-carousel-two owl-theme owl-carousel -->
+            </div><!-- /.auto-container -->
+        </section>
+
+
+        
+        <section class="call-to-section-two alternate">
+            <div class="auto-container">
+                <div class="inner clearfix">
+                    <h2>We’re Ready to Deliver More <br>&amp; Impactful Projects</h2>
+                    <div class="link-box">
+                        <a class="theme-btn btn-style-two" href="contact.php">
+                            <i class="btn-curve"></i>
+                            <span class="btn-title">Contact us NOW!</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Main Footer -->
+        <?php include 'footer.php'; ?>
+
+    </div>
+    <!--End pagewrapper-->
+
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/TweenMax.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/jquery.fancybox.js"></script>
+    <script src="js/owl.js"></script>
+    <script src="js/mixitup.js"></script>
+    <script src="js/knob.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/jQuery.style.switcher.min.js"></script>
+    <script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.2/js.cookie.min.js">
+    </script>
+    <script src="js/jarallax.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/custom-script.js"></script>
+
+
+    <script src="js/lang.js"></script>
+    <script src="../../translate.google.com/translate_a/elementa0d8.html?cb=googleTranslateElementInit"></script>
+    <script src="js/color-switcher.js"></script>
+
+</body>
+
+
+</html>
